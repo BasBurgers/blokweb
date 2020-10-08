64018fc3483030fa005b910e40ab4e21c1a04528
BasBurgers <70704670+BasBurgers@users.noreply.github.com>
2020-10-08 15:30:21 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,15 @@
 // JavaScript Document
+
+function imageGallery(){
+    const highlight = document.querySelector("#schoen1");
+    const previews = document.querySelectorAll("#schoenpreview img");
+
+    previews.forEach(preview =>{
+        preview.addEventListener('click', function(){
+            const smallSrc = this.src;
+            const bigSrc = smallSrc.replace("small", "big")
+            highlight.src = bigSrc;
+        });
+    });
+}
+
