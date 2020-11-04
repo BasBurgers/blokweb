0816afc1465175c2f25c89e7037ac4354ba6f75e
BasBurgers <70704670+BasBurgers@users.noreply.github.com>
2020-11-04 11:40:34 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,48 @@
 // JavaScript Document
 
-function imageGallery(){
-    const highlight = document.querySelector("#schoen1");
-    const previews = document.querySelectorAll("#schoenpreview img");
+/* 0. Elementen selecteren */
+var smallImg1 = document.querySelector('#schoenpreview img:nth-of-type(1)');
 
-    previews.forEach(preview =>{
-        preview.addEventListener('click', function(){
-            const smallSrc = this.src;
-            const bigSrc = smallSrc.replace("small", "big")
-            highlight.src = bigSrc;
-        });
-    });
+var smallImg2 = document.querySelector('#schoenpreview img:nth-of-type(2)');
+
+var smallImg3 = document.querySelector('#schoenpreview img:nth-of-type(3)');
+
+var smallImg4 = document.querySelector('#schoenpreview img:nth-of-type(4)');
+
+var smallImg5 = document.querySelector('#schoenpreview img:nth-of-type(5)');
+
+var bigImg = document.querySelector('#schoen-gallery > img');
+
+/* 1. AddEventListener */
+smallImg1.addEventListener('click', changeImage1);
+
+smallImg2.addEventListener('click', changeImage2);
+
+smallImg3.addEventListener('click', changeImage3);
+
+smallImg4.addEventListener('click', changeImage4);
+
+smallImg5.addEventListener('click', changeImage5);
+
+/* 2. functie aanmaken */
+/* 3. src attribute aanpassen */
+function changeImage1(img) {
+    bigImg.src = '/images/schoen1-big.jpg'
+}
+
+function changeImage2(img) {
+    bigImg.src = '/images/schoen2-big.jpg'
+}
+
+function changeImage3(img) {
+    bigImg.src = '/images/schoen3-big.jpg'
+}
+
+function changeImage4(img) {
+    bigImg.src = '/images/schoen4-big.jpg'
+}
+
+function changeImage5(img) {
+    bigImg.src = '/images/schoen5-big.jpg'
 }
 
